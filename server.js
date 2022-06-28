5ddcd7b2e71b88eb1d1954ba2c11adc643f904b2
Deglas <78974541+Deglas1@users.noreply.github.com>
2022-06-28 10:52:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const aoijs = require("aoi.js");
-const config = require('./handler/config.js');
 const aoimongo = require("aoi.mongo"); 
-const bot = new aoijs.Bot(config.Bot);
+const bot = new aoijs.Bot({
+token: "OTM2OTA2MTUzNTI5NTczMzc2.GRxd6H.uefya2r7hl0HXGGMb1nxl8sHVrfGJtTsFnmZTk",
+   prefix: ["+","<@$clientID>","<@!$clientID>"],
+mobilePlatform: true,
+intents: ["GUILD_MESSAGES", "GUILDS"],   
+suppressAllErrors: false,
+errorMessage: [" ", "{newEmbed:{title: Неизвестная ошибка}{description:При исполнении команды произошла ошибка}{color:RED}}", "{actionRow:{button: Связаться с нами:5:https\\://discord.gg/9U8ny4U3TY}}"],
+database: {
+     db: aoimongo,
+     type: "aoi.mongo",
+     path: "mongodb+srv://ayeban:QwEusLFVJ8wTwwn@djtape.gpkma.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+},    
+ events: { functionError: true} 
+});
 bot.onMessage();
 const loader = new aoijs.LoadCommands(bot)
 loader.load(bot.cmd, "./commands/")
