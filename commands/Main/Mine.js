6cb2a3f865bc8510215b818661c2b7d63c53d2d3
Deglas <78974541+Deglas1@users.noreply.github.com>
2022-06-28 21:34:07 +0300

    Create Mine.js

---
+++ commands/Main/Mine.js
@@ -0,0 +1,9 @@
+module.exports = ({
+name: "mine",
+type: "interaction",
+prototype: "slash",
+code: `$setvar[commands;$sum[$getvar[commands];1]]
+$channelsendmessage[$getvar[logchannel];$guildid {newEmbed:{title: Использована новая команда}{description:**Сервер:** $servername  \n**Пользователь:** $usertag | $authorid \n**Команда:** $commandname}}]
+$interactionReply[;{newEmbed:{author:Работа в глубокой шахте:$useravatar}{description:Ваша задача: $randomText[
+$onlyif[$getglobaluservar[blacklist]==false;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Вы заблокированы, обратитесь на [сервер поддержки]($getvar[invite]) для оказания вам помощи}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]`
+})
