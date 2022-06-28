0edd94b48615e6967bf097222b1e01e5e7f087c9
Deglas <78974541+Deglas1@users.noreply.github.com>
2022-06-28 11:01:36 +0300

    Update botJoins.js

--- handler/botJoins.js
+++ handler/botJoins.js
@@ -3,7 +3,12 @@ bot.guildJoinCommand({
 channel: "$getvar[servers]",
     code: `$title[1;Бота добавили на новый сервер] $description[1;Сервер: $servername | $guildid
 Владелец: $usertag[$ownerid]
-Пользователей: $memberscount] $color[1;GREEN]` });
+Пользователей: $memberscount] $color[1;GREEN]
+$onlyif[$getservervar[blacklist_server]==false;{execute:bot_leave}]` });
+bot.awaitedCommand({
+name: "bot_leave",
+code: `$botLeave`
+})
 bot.guildLeaveCommand({
     channel: "$getvar[servers]",
     code: `$title[1;Бота убрали с сервера] $description[1;Сервер: $servername | $guildid
