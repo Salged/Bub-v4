5d66aad1d8efbb894e9a2b1e26c499cfe9819537
Deglas <78974541+Deglas1@users.noreply.github.com>
2022-06-28 10:24:42 +0300

    Update callbacks.js

--- handler/callbacks.js
+++ handler/callbacks.js
@@ -1,18 +1,5 @@
 module.exports = (bot) => {
-bot.onBanAdd();
-bot.onBanRemove();
 bot.onInteractionCreate();
-bot.onChannelCreate();
-bot.onChannelUpdate();
-bot.onChannelDelete();
-bot.onRoleCreate();
-bot.onRoleUpdate();
-bot.onRoleDelete();
-bot.onMessageUpdate();
-bot.onMessageDelete();
-bot.onVoiceStateUpdate();
 bot.onGuildJoin();
 bot.onGuildLeave();
-bot.onRateLimit();
-
 }
