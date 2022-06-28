bc517fcd9facc6b4d8e9dc4b0d3efd06083f42dc
Deglas <78974541+Deglas1@users.noreply.github.com>
2022-06-28 09:47:59 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const aoijs = require("aoi.js")
 const config = require('./handler/config.js'); //This will be our configuration file. I didn't want your main file be messy. 
 const bot = new aoijs.Bot(config.Bot);
 bot.onMessage();
+bot.onInteractionCreate();
 const loader = new aoijs.LoadCommands(bot)
 loader.load(bot.cmd, "./commands/")
 require('./handler/status')(bot) //This is for bot status file. 
-require('./handler/variables')(bot) //This is for bot variables file. 
-require('./handler/callbacks')(bot)
+require('./handler/variables')(bot)
 require('./handler/antiCrash')(bot) 
 require('./handler/ready')(bot)
 require('./handler/botJoins')(bot)
