7cd7bb91b48220f86e9a1d27abc5138e84ffad3d
Deglas <78974541+Deglas1@users.noreply.github.com>
2022-06-28 09:49:40 +0300

    Update loadCommands.js

--- commands/developer/loadCommands.js
+++ commands/developer/loadCommands.js
@@ -1,13 +1,6 @@
-module.exports = [({
+module.exports = ({
  name: "load",
     aliases: ["loaf", "loax", "loat"],
-code: `lan $updatecommands
-$setvar[botvers2;$sum[$getvar[botvers2];1]]
-$onlyif[$getvar[botvers2]<11;{execute:loadc}]
+code: `💅 $updatecommands
 $onlyforids[$botownerid;]`
-}), ({
-    name: "loadc",
-    type: "awaited",
-    code: `$setvar[botvers;$sum[$getvar[botvers];1]]
-$setvar[botvers2;1]`
-})]
+})
