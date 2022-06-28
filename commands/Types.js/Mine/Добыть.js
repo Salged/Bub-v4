628119bc2e7d2615a378f764e156911b40a1f10e
Deglas <78974541+Deglas1@users.noreply.github.com>
2022-06-28 22:11:58 +0300

    Create Добыть.js

---
+++ commands/Types.js/Mine/Добыть.js
@@ -0,0 +1,12 @@
+module.exports = ({
+type: "interaction",
+prototype: "button",
+code: `$interactionReply[;{newEmbed:{author:$usertag:$useravatar}{description:Вы **** и получили $random[1000;3000]$getvar[wallet] и $random[10;30]xp}{color:$getvar[color]}}] 
+$setuservar[mine_выбор;]
+$setuservar[korm;$sum[$getuservar[korm];$random[1000;3000]]]
+$setuservar[xp;$sum[$getuservar[xp];$random[10;30]]] 
+$onlyif[$getuservar[mine_выбор]==добыть;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Вы должны были выбрать $replacetext[$replacetext[$replacetext[$getuservar[mine_выбор];добыть;⛏️];взорвать;🧨];очистить;🏺]}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
+$onlyIf[$advancedTextSplit[$interactionData[customId];_;2]==$interactionData[author.id];{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Не вы вызвали данную команду}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
+$onlyIf[$advancedTextSplit[$interactionData[customId];_;1]==добыть;] 
+`
+})
