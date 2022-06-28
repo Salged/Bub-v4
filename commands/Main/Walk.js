c4fe20aa4cc656587c7beb5280e2333e44460fa0
Deglas <78974541+Deglas1@users.noreply.github.com>
2022-06-28 15:41:54 +0300

    Create Walk.js

---
+++ commands/Main/Walk.js
@@ -0,0 +1,10 @@
+module.exports = ({
+name: "walk",
+type: "interaction",
+prototype: "slash",
+code: `$setvar[commands;$sum[$getvar[commands];1]]
+$channelsendmessage[$getvar[logchannel];$guildid {newEmbed:{title: Использована новая команда}{description:**Сервер:** $servername  \n**Пользователь:** $usertag | $authorid \n**Команда:** $commandname}}]
+$interactionReply[Вы погуляли с $getuservar[name] и повысили настроение! (+$random[15;30]xp);;;$randomText[https://i.imgur.com/HEkXJsB.jpg;https://i.imgur.com/4D8jqpM.jpg]]
+$setuservar[xp;$sum[$getuservar[xp];$random[15;30]]]
+$onlyif[$getglobaluservar[blacklist]==false;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Вы заблокированы, обратитесь на [сервер поддержки]($getvar[invite]) для оказания вам помощи}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]`
+})
