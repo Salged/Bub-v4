c8ca9ff7db2329c33bcccb21fce0b8ac7338c62f
Deglas <78974541+Deglas1@users.noreply.github.com>
2022-06-28 10:59:31 +0300

    Update variables.js

--- handler/variables.js
+++ handler/variables.js
@@ -2,6 +2,12 @@ module.exports= (bot) =>{
 bot.variables({ 
 logchannel: "945968304672083978",
 servers: "945968304672083978",
-korm: "100"
+korm: "100",
+wallet: "<:bubcoin:836289342593826886>",
+blacklist: "false",
+blacklist_count: "0",
+blacklist_users: "",
+blacklist_server: "false",
+blacklist_servers: ""
 })
 }
