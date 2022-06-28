49775c1fd4dba633bd102dd053e8d0361d237023
Deglas <78974541+Deglas1@users.noreply.github.com>
2022-06-28 09:46:39 +0300

    Update config.js

--- handler/config.js
+++ handler/config.js
@@ -1,12 +1,12 @@
 module.exports = {
     Bot: {
-    token: "OTgxNjU2MTgxODYyOTg1NzU5.Gaus36.deqhdqQmP9_xcydthyqEdT5LhFDwPKPKC3s580",
+    token: "OTM2OTA2MTUzNTI5NTczMzc2.YfT_2Q.W4g2dNbHz_mZ5LS1x0qKlBjxJvc",
 
-   prefix: ["q!","<@$clientID>","<@!$clientID>"],
+   prefix: ["+","<@$clientID>","<@!$clientID>"],
 
 mobilePlatform: true,
 
-intents: ["GUILD_MESSAGES", "GUILDS", "GUILD_VOICE_STATES", "GUILD_INVITES", "GUILD_WEBHOOKS", "GUILD_BANS", "GUILD_EMOJIS_AND_STICKERS"],
+intents: ["GUILD_MESSAGES", "GUILDS"]
     
 suppressAllErrors: false,
 errorMessage: [" ", "{newEmbed:{title: Неизвестная ошибка}{description:При исполнении команды произошла ошибка}{color:RED}}", "{actionRow:{button: Связаться с нами:5:https\\://discord.gg/9U8ny4U3TY}}"],
