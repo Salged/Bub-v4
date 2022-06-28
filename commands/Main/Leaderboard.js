4ab352dff42a0d2a038f745b1c623b5fe798011b
Deglas <78974541+Deglas1@users.noreply.github.com>
2022-06-28 11:10:03 +0300

    Update Leaderboard.js

--- commands/Main/Leaderboard.js
+++ commands/Main/Leaderboard.js
@@ -2,4 +2,6 @@ module.exports = ({
 name: "leaderboard",
 type: "interaction",
 prototype: "slash",
-code: `$interactionReply[$replacetext[$replacetext[$replacetext[$replacetext[$userLeaderboard[$guildid;korm;asc;{top} - {username}: {value}$getservervar[wallet]];1 -;🥇 -;1];2 -;🥈 -;1];3 -;🥉 -;1];`;]
+code: `$interactionReply[$replacetext[$replacetext[$replacetext[$replacetext[$userLeaderboard[$guildid;korm;asc;{top} - {username}: {value}$getservervar[wallet]];1 -;🥇 -;1];2 -;🥈 -;1];3 -;🥉 -;1];`;]]
+`
+})
