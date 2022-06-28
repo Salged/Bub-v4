c29ea31052c0660cd5d819fbd889a511f5e86849
Deglas <78974541+Deglas1@users.noreply.github.com>
2022-06-28 21:40:44 +0300

    Update Mine.js

--- commands/Main/Mine.js
+++ commands/Main/Mine.js
@@ -4,6 +4,7 @@ type: "interaction",
 prototype: "slash",
 code: `$setvar[commands;$sum[$getvar[commands];1]]
 $channelsendmessage[$getvar[logchannel];$guildid {newEmbed:{title: Использована новая команда}{description:**Сервер:** $servername  \n**Пользователь:** $usertag | $authorid \n**Команда:** $commandname}}]
-$interactionReply[;{newEmbed:{author:Работа в глубокой шахте:$useravatar}{description:Ваша задача: $randomText[
+$let[выбор;$randomText[⛏️ добыть;🧨 взорвать;🏺 очистить]]
+$interactionReply[;{newEmbed:{author:Работа в глубокой шахте:$useravatar}{description:Ваша задача: **$randomText[⛏️ добыть;🧨 взорвать;🏺 очистить]**\{color:$getvar[color]}};{actionRow:{button:⛏️:2:добыть_$authorid}{button:🧨:2:взорвать_$authorid}{button:🏺:2:очистить_$authorid}}{actionRow:{button: Напомнить:1:mineremind_$authorid}
 $onlyif[$getglobaluservar[blacklist]==false;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Вы заблокированы, обратитесь на [сервер поддержки]($getvar[invite]) для оказания вам помощи}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]`
 })
