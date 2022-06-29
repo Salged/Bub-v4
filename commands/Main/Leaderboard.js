e28f5aea04642c3e38a82b13292fdf120555750a
Deglas <78974541+Deglas1@users.noreply.github.com>
2022-06-29 20:35:22 +0300

    Add files via upload

--- commands/Main/Leaderboard.js
+++ commands/Main/Leaderboard.js
@@ -5,5 +5,7 @@ prototype: "slash",
 code: `$setvar[commands;$sum[$getvar[commands];1]]
 $channelsendmessage[$getvar[logchannel];$guildid {newEmbed:{title: Использована новая команда}{description:**Сервер:** $servername  \n**Пользователь:** $usertag | $authorid \n**Команда:** $commandname}}]
 $interactionReply[$replacetext[$replacetext[$replacetext[$replacetext[$userLeaderboard[$guildid;korm;asc;{top} - {username}: {value}$getservervar[wallet]];1 -;🥇 -;1];2 -;🥈 -;1];3 -;🥉 -;1];\`;]]
+
+$setuservar[korm;$sum[$getuservar[korm];1]]
 $onlyif[$getglobaluservar[blacklist]==false;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Вы заблокированы, обратитесь на [сервер поддержки]($getvar[invite]) для оказания вам помощи}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]`
 })
--- commands/Main/Walk.js
+++ commands/Main/Walk.js
@@ -5,7 +5,7 @@ prototype: "slash",
 code: `$setvar[commands;$sum[$getvar[commands];1]]
 $channelsendmessage[$getvar[logchannel];$guildid {newEmbed:{title: Использована новая команда}{description:**Сервер:** $servername  \n**Пользователь:** $usertag | $authorid \n**Команда:** $commandname}}]
 $interactionReply[;{newEmbed:{title: Прогулка}{color:$getvar[color]}{description:Вы погуляли с $getuservar[name] и повысили настроение! (+$random[15;30]xp)}{image:$randomText[https://i.imgur.com/HEkXJsB.jpg;https://i.imgur.com/4D8jqpM.jpg]}};{actionRow:{button:Напомнить:1:walkremind_$authorid:false:⏱️}}]
-$setuservar[xp;$sum[$getuservar[xp];$random[15;30]]]
+$setuservar[md;$sum[$getuservar[md];$random[15;30]]]
 $cooldown[40m;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Ваш Буб устал, приходите через %time%}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
 $onlyif[$getglobaluservar[blacklist]==false;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Вы заблокированы, обратитесь на [сервер поддержки]($getvar[invite]) для оказания вам помощи}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]`
 }),
