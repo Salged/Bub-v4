19c7e0a5746fc74002b6755a9aa03d4d44bfb5f7
Deglas <78974541+Deglas1@users.noreply.github.com>
2022-06-29 21:01:19 +0300

    Update Очистить.js

--- commands/Types/Mine/Очистить.js
+++ commands/Types/Mine/Очистить.js
@@ -6,7 +6,7 @@ $setuservar[mine_выбор;]
 $setuservar[korm;$sum[$getuservar[korm];$random[1000;3000]]]
 $setuservar[xp;$sum[$getuservar[xp];$random[10;30]]] 
 $onlyif[$getuservar[mine_выбор]==очистить;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Вы должны были выбрать $replacetext[$replacetext[$replacetext[$getuservar[mine_выбор];добыть;⛏️];взорвать;🧨];очистить;🧹]}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
-$onlyif[$getuservar[mine_выбор]!=;"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:На данный момент для вас нет работы!}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
+$onlyif[$getuservar[mine_выбор]!=;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:На данный момент для вас нет работы!}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
 $onlyIf[$advancedTextSplit[$interactionData[customId];_;2]==$interactionData[author.id];{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Не вы вызвали данную команду}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
 $onlyIf[$advancedTextSplit[$interactionData[customId];_;1]==очистить;] 
 
