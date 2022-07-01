7db91524139633b8e8a4074d0bddbfe3261e26e6
Deglas <78974541+Deglas1@users.noreply.github.com>
2022-07-01 18:59:05 +0300

    Update Rank.js

--- commands/Levels/Rank.js
+++ commands/Levels/Rank.js
@@ -1,21 +1,10 @@
 module.exports = ({
-name: "rankk",
-code: `
-$djsEval[
-  const Discord = require ('discord.js');
-  const canvacord = require('canvacord');
-  const rank = new canvacord.Rank()
-  .setRank($getLeaderboardInfo[lvl;$findUser[$message];user;top])
- .setStatus("$status[$findUser[$message]]")
-  .setAvatar("$replaceText[$replaceText[$userAvatar[$findUser[$message]];.webp;.png];?size=2048;]")
-  .setBackground("IMAGE", "https://media.discordapp.net/attachments/840523345132912640/861260288824705044/20210704_175957.jpg")
-  .setCurrentXP($getUserVar[xp;$findUser[$message]])
-  .setRequiredXP($getUserVar[sledxp;$findUser[$message]])
-  .setStatus("$status[$findUser[$message]]")
-  .setLevel($getUserVar[lvl;$findUser[$message]])
-  .setProgressBar("#$getvar[color]", "COLOR")
-  .setUsername("$nickname[$findUser[$message]]")
-  .setDiscriminator("$discriminator[$findUser[$message]]");
-  rank.build();yes]
-`
+name: "rank",
+type: "interaction",
+prototype: "slash",
+code: `$setvar[commands;$sum[$getvar[commands];1]]
+$channelsendmessage[$getvar[logchannel];$guildid {newEmbed:{title: Использована новая команда}{description:**Сервер:** $servername  \n**Пользователь:** $usertag | $authorid \n**Команда:** $commandname}}]
+$interactionReply[;{newEmbed:{title:Карточка $usertag[$get[id]]}{image:https://vacefron.nl/api/rankcard?username=$replaceText[$username[$get[id]]; ;+]&avatar=$userAvatar[$get[id]]?size=4096&level=$getUserVar[lvl;$get[id]]&rank=&currentxp=$getUserVar[xp;$get[id]]&nextlevelxp=$getUserVar[sledxp;$get[id]]&previouslevelxp=0&custombg=https://i.imgur.com/EBPIwMZ.jpeg&xpcolor=$getvar[color]&isboosting=$isBoosting[$get[id]]}{color:$getvar[color]}}]
+$let[id;$replacetext[$interactiondata[options.data[0].value];undefined;$authorid]]
+$onlyif[$getglobaluservar[blacklist]==false;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Вы заблокированы, обратитесь на [сервер поддержки]($getvar[invite]) для оказания вам помощи}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]`
 })
