68530ac5c650d655748e1d52f25597b479a24136
Deglas <78974541+Deglas1@users.noreply.github.com>
2022-07-01 11:35:54 +0300

    Update builder.js

--- commands/Main/builder.js
+++ commands/Main/builder.js
@@ -4,9 +4,8 @@ type: "interaction",
 prototype: "slash",
 code: `$setvar[commands;$sum[$getvar[commands];1]]
 $channelsendmessage[$getvar[logchannel];$guildid {newEmbed:{title: Использована новая команда}{description:**Сервер:** $servername  \n**Пользователь:** $usertag | $authorid \n**Команда:** $commandname}}]
-$setuservar[build_выбор;$randomText[добыть;взорвать;очистить]]
-$interactionReply[;{newEmbed:{author:Работа на строй площадке:$useravatar}{field:Ваша задача#COLON# **$replaceText[$replaceText[$replaceText[$randomText[кирпич;техника;покурить];кирпич;класть кирпич];техника;занести технику];покурить;пойти покурить]**:\n🧱 — класть кирпичи\n💻 — занести технику\n🚬 — пойти покурить}{color:$getvar[color]}};{actionRow:{button:🧱:2:кирпич_$authorid}{button:🖥️:2:техника_$authorid}{button:🚬:2:о
-покурить_$authorid}}{actionRow:{button: Напомнить:1:buildremind_$authorid}}]
+$setuservar[build_выбор;$randomText[кирпич;техника;покурить]]
+$interactionReply[;{newEmbed:{author:Работа на строй площадке:$useravatar}{field:Ваша задача#COLON# **$replaceText[$replaceText[$replaceText[$randomText[кирпич;техника;покурить];кирпич;класть кирпич];техника;занести технику];покурить;пойти покурить]**:\n🧱 — класть кирпичи\n💻 — занести технику\n🚬 — пойти покурить}{color:$getvar[color]}};{actionRow:{button:🧱:2:кирпич_$authorid}{button:🖥️:2:техника_$authorid}{button:🚬:2:покурить_$authorid}}{actionRow:{button: Напомнить:1:buildremind_$authorid}}]
 $cooldown[50m;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Ваш Буб устал, приходите через %time%}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
 $onlyif[$getuservar[lvl]>=2;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:У вашего Буба должен быть 2 уровень и выше}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
 $onlyif[$getglobaluservar[blacklist]==false;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Вы заблокированы, обратитесь на [сервер поддержки]($getvar[invite]) для оказания вам помощи}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]`
