e0460dfa2cb8553c6d597f7e5b93425a297a6b34
Deglas <78974541+Deglas1@users.noreply.github.com>
2022-07-01 23:04:51 +0300

    Update Дракон Буб .js

--- commands/Skins/Дракон Буб .js
+++ commands/Skins/Дракон Буб .js
@@ -11,7 +11,7 @@ prototype: "button",
 code: `$setuservar[img;https://media.discordapp.net/attachments/839587792786161664/951836635551637514/IMG_20220311_093636.jpg]
 $setuservar[скин_2;https://media.discordapp.net/attachments/839587792786161664/951836635551637514/IMG_20220311_093636.jpg]
 $interactionReply[Поздравляю вас с покупкой скина **#2️⃣**]
-$setuservar[korm;$sub[$getuservar[korm];]]
+$setuservar[korm;$sub[$getuservar[korm];35000]]
 $onlyif[$getuservar[скин_2]==;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:У вас уже приобретён данный скин}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
 $onlyif[$getuservar[korm]>=35000;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:У вас на балансе слишком мало средств}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
 `
