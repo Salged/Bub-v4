a3e97984b595a57e98ed5b9b7e9902c47e5ea4cf
Deglas <78974541+Deglas1@users.noreply.github.com>
2022-07-01 16:45:28 +0300

    Create Levelup.js

---
+++ commands/Levels/Levelup.js
@@ -0,0 +1,11 @@
+module.exports = ({
+name: "$alwaysExecute",
+code: `
+**$userTag[$authorID]**, ты повысил свой лвл до **$getUserVar[lvl]** <:bubkrytoi:838837227609522226>
+
+
+$setUserVar[lvl;$sum[$getUserVar[lvl];1]]
+$setUserVar[xp;0]
+$setUserVar[sledxp;$sum[$getUserVar[sledxp];150]]
+$onlyIf[$getUserVar[xp]>=$getUserVar[sledxp];]`
+})
