a0f65499885485765005f1d91a8c24013a9710a5
Deglas <78974541+Deglas1@users.noreply.github.com>
2022-07-01 21:50:50 +0300

    Update Bub.js

--- commands/Main/Bub.js
+++ commands/Main/Bub.js
@@ -6,7 +6,9 @@ code: `$setvar[commands;$sum[$getvar[commands];1]]
 $channelsendmessage[$getvar[logchannel];$guildid {newEmbed:{title: Использована новая команда}{description:**Сервер:** $servername  \n**Пользователь:** $usertag | $authorid \n**Команда:** $commandname}}]
 $interactionReply[;{newEmbed:{title:Профиль $username[$get[id]]}{thumbnail:$userAvatar[$get[id]]}{description:❤️ ・ Имя: **$getUserVar[name;$get[id]]**
 $getVar[wallet] ・ Бубкойны: **$numberSeparator[$getUserVar[korm;$get[id]];.]**
-🌈 ・ Настроение: **$replaceText[$replaceText[$checkCondition[$getUserVar[md;$get[id]]<150];true;Плохое];false;Хорошее]**}{color:$getvar[color]}}]
+🌈 ・ Настроение: **$replaceText[$replaceText[$checkCondition[$getUserVar[md;$get[id]]<150];true;Плохое];false;Хорошее]**
+🛡️ ・ Лвл: **$numberSeparator[$getUserVar[lvl;$get[id]];.]**
+⚗️ ・ Xp: **$numberSeparator[$getUserVar[xp;$get[id]];.]/$numberSeparator[$getUserVar[sledxp;$get[id]];.]**}{color:$getvar[color]}}]
 $let[id;$replacetext[$interactiondata[options.data[0].value];undefined;$authorid]]
 $onlyif[$getglobaluservar[blacklist]==false;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Вы заблокированы, обратитесь на [сервер поддержки]($getvar[invite]) для оказания вам помощи}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]`
 })
