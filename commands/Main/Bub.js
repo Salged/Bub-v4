df28aace58c98a4cffa7b6c84dae914b543da1fa
Deglas <78974541+Deglas1@users.noreply.github.com>
2022-07-02 14:30:41 +0300

    Update Bub.js

--- commands/Main/Bub.js
+++ commands/Main/Bub.js
@@ -6,7 +6,7 @@ code: `$setvar[commands;$sum[$getvar[commands];1]]
 $channelsendmessage[$getvar[logchannel];$guildid {newEmbed:{title: Использована новая команда}{description:**Сервер:** $servername  \n**Пользователь:** $usertag | $authorid \n**Команда:** $commandname}}]
 $interactionReply[;{newEmbed:{title:$replacetext[$replacetext[$getservervar[lang];ru;Профиль];en;Profile] $username[$get[id]]}{thumbnail:$userAvatar[$get[id]]}{description:❤️ ・ $replacetext[$replacetext[$getservervar[lang];ru;Имя];en;Name]: **$getUserVar[name;$get[id]]**
 $getVar[wallet] ・ $replacetext[$replacetext[$getservervar[lang];ru;Бубкойны];en;Bubcoins]: **$numberSeparator[$getUserVar[korm;$get[id]];.]**
-🌈 ・ $replacetext[$replacetext[$getservervar[lang];ru;Настроение];en;Mood]: **$replaceText[$replaceText[$checkCondition[$getUserVar[md;$get[id]]<150];true;Плохое];false;Хорошее]** ($getuservar[md]/150)
+🌈 ・ $replacetext[$replacetext[$getservervar[lang];ru;Настроение];en;Mood]: **$replaceText[$replaceText[$checkCondition[$getUserVar[md;$get[id]]<150];true;$replacetext[$replacetext[$getservervar[lang];ru;Плохое];en;bad]];false;$replacetext[$replacetext[$getservervar[lang];ru;Хорошее];en;good]]** ($getuservar[md]/150)
 🛡️ ・ $replacetext[$replacetext[$getservervar[lang];ru;Лвл];en;Level]: **$numberSeparator[$getUserVar[lvl;$get[id]];.]**
 ⚗️ ・ Xp: **$numberSeparator[$getUserVar[xp;$get[id]];.]/$numberSeparator[$getUserVar[sledxp;$get[id]];.]**}{color:$getvar[color]}{image:$getuservar[img]}}]
 $let[id;$replacetext[$interactiondata[options.data[0].value];undefined;$authorid]]
