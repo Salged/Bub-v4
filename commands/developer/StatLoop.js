67e39251c4edce82ea099eb0be1249b671ae6202
Deglas <78974541+Deglas1@users.noreply.github.com>
2022-07-02 19:03:45 +0300

    Update StatLoop.js

--- commands/developer/StatLoop.js
+++ commands/developer/StatLoop.js
@@ -4,7 +4,7 @@ channel: "975806804716032020",
 code: `
 $editMessage[991407327838220320;
 {newEmbed:{author: Статистика бота:$useravatar[$clientid]}
-{description: **RAM:** $ram MB / $maxRam MB\n**CPU:** $cpu% / 100%}
+{description: **RAM:** $ram MB / $maxRam MB\n**Серверов:** $serverCount \n**Пользователей:** $allmemberscount}
 {color:$replaceText[$userRoleColor[$clientID];#;]}};975806804716032020]
 $suppressErrors`,
 executeOnStartup: true,
