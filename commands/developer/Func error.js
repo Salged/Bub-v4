6ad16c0167e1f44b1a88ecd7e57ab66435bb90f9
Deglas <78974541+Deglas1@users.noreply.github.com>
2022-07-02 19:22:58 +0300

    Create Func error.js

---
+++ commands/developer/Func error.js
@@ -0,0 +1,14 @@
+module.exports = {
+name: "error",
+type: "functionError",
+channel: "838794366423662672",
+code: `
+$setvar[commandsError;$sum[$getvar[commandsError];1]]
+$channelSendMessage[838794366423662672;<@$botownerid>
+$guildid {newEmbed:{title:Была обнаружена ошибка!}{color:RED}{description:Команда которую использовали при ошибке - \`$handlEerror[command]\`
+Сервер — $serverName
+Функция в которой есть ошибка - \`$handleerror[function]\`
+Ошибка - $handleError[error]
+}}]`
+}
+
