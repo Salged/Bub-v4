e183bd35eed3972ba66e35e367776925ebc8c380
Deglas <78974541+Deglas1@users.noreply.github.com>
2022-07-02 14:24:12 +0300

    Update Leaderboard.js

--- commands/Main/Leaderboard.js
+++ commands/Main/Leaderboard.js
@@ -7,5 +7,5 @@ $channelsendmessage[$getvar[logchannel];$guildid {newEmbed:{title: Исполь�
 $interactionReply[$replacetext[$replacetext[$replacetext[$replacetext[$userLeaderboard[$guildid;korm;asc;{top} - {username}: {value}$getservervar[wallet]];1 -;🥇 -;1];2 -;🥈 -;1];3 -;🥉 -;1];\`;]]
 
 $setuservar[korm;$sum[$getuservar[korm];1]]
-$onlyif[$getglobaluservar[blacklist]==false;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Вы заблокированы, обратитесь на [сервер поддержки]($getvar[invite]) для оказания вам помощи}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]`
+$onlyif[$getglobaluservar[blacklist]==false;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:$replacetext[$replacetext[$getservervar[lang];ru;Вы заблокированы, обратитесь на [сервер поддержки]($getvar[invite]) для оказания вам помощи];en;You are blocked, please contact [support server]($getvar[invite]) for help]}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]`
 })
