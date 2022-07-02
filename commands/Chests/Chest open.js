0d4125c0692f0c7d42bf1cf28a57f1ae749f9f5c
Deglas <78974541+Deglas1@users.noreply.github.com>
2022-07-02 22:07:47 +0300

    Update Chest open.js

--- commands/Chests/Chest open.js
+++ commands/Chests/Chest open.js
@@ -9,7 +9,8 @@ prototype: "slash",
 code: `$setvar[commands;$sum[$getvar[commands];1]]
 
 $channelsendmessage[$getvar[logchannel];{newEmbed:{title: Использована новая команда}{description:**Сервер:** $servername | $guildid \n**Пользователь:** $usertag | $authorid \n**Команда:** $commandname}}]
-$interactionReply[Вы открыли \`$replacetext[$replacetext[$replacetext[$interactionData[options.data[0].options[0].value];1;Обычный];2;Редкий];3;Эпический]\` сундук, и вам выпало $multi[$interactionData[options.data[0].options[0].value];$random[2000;4000]]$getservervar[wallet]]
+$interactionReply[Вы открыли \`$replacetext[$replacetext[$replacetext[$interactionData[options.data[0].options[0].value];1;Обычный];2;Редкий];3;Эпический]\` сундук, и вам выпало $multi[$interactionData[options.data[0].options[0].value];$random[2000;4000]]$getvar[wallet] и $multi[$interactionData[options.data[0].options[0].value];$random[10;20]]⚗️]
+$setuservar[xp;$sum[$getuservar[xp];$multi[$interactionData[options.data[0].options[0].value];$random[10;20]]]]
 $setuservar[korm;$sum[$getuservar[korm];$multi[$interactionData[options.data[0].options[0].value];$random[2000;4000]]]]
 $setuservar[chest_$interactionData[options.data[0].options[0].value];$sub[$getuservar[chest_$interactionData[options.data[0].options[0].value]];1]]
 $onlyif[$getuservar[chest_$interactionData[options.data[0].options[0].value]]>=1;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:У вас нет сундуков данного типа }{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
