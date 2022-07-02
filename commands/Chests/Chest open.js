59952193406662f53eab253f5772a5e825ad453b
Deglas <78974541+Deglas1@users.noreply.github.com>
2022-07-02 22:05:38 +0300

    Add files via upload

---
+++ commands/Chests/Chest open.js
@@ -0,0 +1,19 @@
+module.exports = ({
+
+name: "chest",
+
+type: "interaction",
+
+prototype: "slash",
+
+code: `$setvar[commands;$sum[$getvar[commands];1]]
+
+$channelsendmessage[$getvar[logchannel];{newEmbed:{title: Использована новая команда}{description:**Сервер:** $servername | $guildid \n**Пользователь:** $usertag | $authorid \n**Команда:** $commandname}}]
+$interactionReply[Вы открыли \`$replacetext[$replacetext[$replacetext[$interactionData[options.data[0].options[0].value];1;Обычный];2;Редкий];3;Эпический]\` сундук, и вам выпало $multi[$interactionData[options.data[0].options[0].value];$random[2000;4000]]$getservervar[wallet]]
+$setuservar[korm;$sum[$getuservar[korm];$multi[$interactionData[options.data[0].options[0].value];$random[2000;4000]]]]
+$setuservar[chest_$interactionData[options.data[0].options[0].value];$sub[$getuservar[chest_$interactionData[options.data[0].options[0].value]];1]]
+$onlyif[$getuservar[chest_$interactionData[options.data[0].options[0].value]]>=1;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:У вас нет сундуков данного типа }{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
+$onlyif[$getglobaluservar[blacklist]==false;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Вы заблокированы, обратитесь на [сервер поддержки]($getvar[invite]) для оказания вам помощи}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
+$onlyif[$interactionData[options._subcommand]==open;]`
+
+})
