d2b11dae60446070d9538ebdfc0b51f60f4886df
Deglas <78974541+Deglas1@users.noreply.github.com>
2022-07-02 22:29:53 +0300

    Update and rename Rob (2).js to Rob.js

--- commands/Main/Rob.js
+++ commands/Main/Rob.js
@@ -4,24 +4,18 @@ module.exports = [({
     type: "interaction",
     prototype: "slash",
     code: `$setvar[commands;$sum[$getvar[commands];1]]
-
 $channelsendmessage[$getvar[logchannel];{newEmbed:{title: Использована новая команда}{description:**Сервер:** $servername | $guildid \n**Пользователь:** $usertag | $authorid \n**Команда:** $commandname}}]
 $if[$randomtext[1;2;1;2]==1]
 $setuservar[korm;$sum[$getuservar[korm];$get[amount]]]
 $setuservar[korm;$sub[$getuservar[korm;$interactiondata[options.data[0].value]];$get[amount]];$interactiondata[options.data[0].value]]
-$interactionReply[;{newEmbed:{title: Ограбление}{description:$usertag украл у <@$interactiondata[options.data[0].value]> $get[amount]$getservervar[wallet]}{color:GREEN}};{actionRow:{button: Напомнить:1:robremind_$authorid:false:⏱️}}]
-
-    
+$interactionReply[;{newEmbed:{title: Ограбление}{description:$usertag украл у <@$interactiondata[options.data[0].value]> $get[amount]$getservervar[wallet]}{color:GREEN}};{actionRow:{button: Напомнить:1:robremind_$authorid:false:⏱️}}]   
 $elseif[$randomtext[1;2;1;2]==2]
 $setuservar[korm;$sub[$getuservar[korm];$get[rob]]]
-
 $setuservar[korm;$sum[$getuservar[korm;$interactiondata[options.data[0].value]];$get[rob]];$interactiondata[options.data[0].value]]
-
 $interactionReply[;{newEmbed:{title: Ограбление}{description:$usertag попытался украсть деньги у <@$interactiondata[options.data[0].value]>, но был пойман и заплатил штраф $get[rob]$getservervar[wallet]}{color:RED}};{actionRow:{button: Напомнить:1:robremind_$authorid:false:⏱️}}]
 $let[rob;$round[$divide[$get[amount];2]]]
 $endelseif
 $endif
-
 
 $let[amount;$round[$multi[$random[5;15];$get[money]]]]
 $let[money;$round[$divide[$getuservar[korm;$interactiondata[options.data[0].value]];100]]]
@@ -30,7 +24,6 @@ $onlyif[$getuservar[korm]>=500;{"embeds": "{newEmbed:{thumbnail:https://cdn.disc
 $onlyif[$getuservar[korm;$interactionData[options.data[0].value]]>=500;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:У пользователя которого вы хотите ограбить на руках должны быть 500$getvar[wallet]}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
 $onlyif[$memberexists[$interactiondata[options.data[0].value]]==true;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Пользователя нет на сервере}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
 $onlyif[$interactiondata[options.data[0].value]!=$authorid;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Нельзя грабить самого себя}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
-
 
 $onlyif[$getglobaluservar[blacklist]==false;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Вы заблокированы, обратитесь на [сервер поддержки]($getvar[invite]) для оказания вам помощи}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]`
     }),
@@ -54,7 +47,7 @@ $onlyIf[$advancedTextSplit[$interactionData[customId];_;1]==robremind;]`
 
     type: "timeout",
 
-    code: `$sendDM[Команда \`rob\`снова доступна!;$timeoutdata[userID]]
+    code: `$sendDM[Команда \`rob\` снова доступна!;$timeoutdata[userID]]
 
 $onlyif[$isuserdmenabled[$timeoutdata[userID]]==true;]`
 
