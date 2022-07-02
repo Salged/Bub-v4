1555f3d1e94cc0006fda49e7b61134a27841d003
Deglas <78974541+Deglas1@users.noreply.github.com>
2022-07-02 19:48:53 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ token: "OTM2OTA2MTUzNTI5NTczMzc2.GRxd6H.uefya2r7hl0HXGGMb1nxl8sHVrfGJtTsFnmZTk",
    prefix: ["+","<@$clientID>","<@!$clientID>"],
 mobilePlatform: true,
 intents: ["GUILD_MESSAGES", "GUILDS"],   
-suppressAllErrors: false,
+suppressAllErrors: true,
 errorMessage: [" ", "{newEmbed:{title: Неизвестная ошибка}{description:При исполнении команды произошла ошибка}{color:RED}}", "{actionRow:{button: Связаться с нами:5:https\\://discord.gg/nmcEtFs35w}}"],
 database: {
      db: aoimongo,
