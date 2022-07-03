892bd039efa6ac103e3d954fe7860653daae7f26
Deglas <78974541+Deglas1@users.noreply.github.com>
2022-07-03 16:14:54 +0300

    Update Болотный буб.js

--- commands/Skins/Болотный буб.js
+++ commands/Skins/Болотный буб.js
@@ -10,7 +10,7 @@ type: "interaction",
 prototype: "button",
 code: `$setuservar[img;https://media.discordapp.net/attachments/937714691105517619/937715867171557396/PicsArt_01-31-05.23.15.jpg]
 $setuservar[скин_3;https://media.discordapp.net/attachments/937714691105517619/937715867171557396/PicsArt_01-31-05.23.15.jpg]
-$interactionReply[Поздравляю вас с покупкой скина **#**]
+$interactionReply[Поздравляю вас с покупкой скина **#3️⃣**]
 $setuservar[korm;$sub[$getuservar[korm];50000]]
 $onlyif[$getuservar[скин_3]==;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:У вас уже приобретён данный скин}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
 $onlyif[$getuservar[korm]>=50000;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:У вас на балансе слишком мало средств}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
