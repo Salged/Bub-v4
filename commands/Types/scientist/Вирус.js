87c69d8907ee39929adbf4a102981eff451a6f1d
Deglas <78974541+Deglas1@users.noreply.github.com>
2022-07-06 22:12:02 +0300

    Update Вирус.js

--- commands/Types/scientist/Вирус.js
+++ commands/Types/scientist/Вирус.js
@@ -8,6 +8,6 @@ $setuservar[xp;$sum[$getuservar[xp];$random[20;45]]]
 $onlyif[$getuservar[учёный_выбор]==вирус;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title:Произошла ошибка!}{description:Вы должны были выбрать $replacetext[$replacetext[$replacetext[$getuservar[учёный_выбор];вакцина;⚗️];расчёт;📊];вирус;🦠]}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
 $onlyif[$getuservar[учёный_выбор]!=;"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title:Произошла ошибка!}{description:На данный момент для вас нет работы!}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
 $onlyIf[$advancedTextSplit[$interactionData[customId];_;2]==$interactionData[author.id];{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title:Произошла ошибка!}{description:Не вы вызвали данную команду}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
-$onlyIf[$advancedTextSplit[$interactionData[customId];_;1]==;] 
+$onlyIf[$advancedTextSplit[$interactionData[customId];_;1]==вирус;] 
 `
 })
