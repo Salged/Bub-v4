10b4ba5230e9a86f02e336d05a11d539cf5bb91a
Deglas <78974541+Deglas1@users.noreply.github.com>
2022-07-06 22:26:32 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const aoijs = require("aoi.js");
-const aoimongo = require("aoi.mongo"); 
+
 const bot = new aoijs.Bot({
 token: "OTM2OTA2MTUzNTI5NTczMzc2.GRxd6H.uefya2r7hl0HXGGMb1nxl8sHVrfGJtTsFnmZTk",
    prefix: ["+","<@$clientID>","<@!$clientID>"],
@@ -7,14 +7,17 @@ mobilePlatform: true,
 intents: ["GUILD_MESSAGES", "GUILDS"],   
 suppressAllErrors: true,
 errorMessage: [" ", "{newEmbed:{title: Неизвестная ошибка}{description:При исполнении команды произошла ошибка}{color:RED}}", "{actionRow:{button: Связаться с нами:5:https\\://discord.gg/nmcEtFs35w}}"],
-database: {
-     db: aoimongo,
-     type: "aoi.mongo",
-     path: "mongodb+srv://ayeban:QwEusLFVJ8wTwwn@djtape.gpkma.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-},    
- events: { functionError: true} 
+events: { functionError: true} 
 });
 bot.onMessage();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://ayeban:QwEusLFVJ8wTwwn@djtape.gpkma.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 const loader = new aoijs.LoadCommands(bot)
 loader.load(bot.cmd, "./commands/")
 require('./handler/status')(bot) //This is for bot status file. 
