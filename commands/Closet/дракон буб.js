2e9575c3de1ed2b98676c6271e133e2568543dfc
Salged <1212discord@gmail.com>
2026-07-25 23:17:15 +0300

    Create дракон буб.js

---
+++ commands/Closet/дракон буб.js
@@ -0,0 +1,10 @@
+module.exports = ({
+name: "2️⃣2️⃣",
+type: "interaction",
+prototype: "button",
+code: `$setuservar[img;$getuservar[скин_$get[скин]]]
+$interactionReply[{newEmbed:{title:Вы успешно надели скин #2️⃣}{image:$getuservar[скин_$get[скин]]}{color:$getvar[color]}}]
+
+$onlyif[$getuservar[скин_$get[скин]]!=;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:У вас не куплен данный скин}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
+$let[скин;2]`
+})
