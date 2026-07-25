b78b3645bcc9d2024ff6cc8e8fab0059b31041c7
Salged <1212discord@gmail.com>
2026-07-25 22:32:39 +0300

    Update Chest open.js

--- commands/Chests/Chest open.js
+++ commands/Chests/Chest open.js
@@ -1,11 +1,7 @@
 module.exports = ({
-
 name: "chest",
-
 type: "interaction",
-
 prototype: "slash",
-
 code: `$setvar[commands;$sum[$getvar[commands];1]]
 
 $channelsendmessage[$getvar[logchannel];{newEmbed:{title: Использована новая команда}{description:**Сервер:** $guildname | $guildid \n**Пользователь:** $usertag | $authorid \n**Команда:** $commandname}}]
