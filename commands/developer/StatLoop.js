680661b65b8b4abef2821cff60b2867e588fca19
Salged <1212discord@gmail.com>
2026-07-26 01:09:15 +0300

    Update StatLoop.js

--- commands/developer/StatLoop.js
+++ commands/developer/StatLoop.js
@@ -5,7 +5,7 @@ code: `
 $editMessage[1530667926242328607;
 {newEmbed:{author: Статистика бота:$useravatar[$clientid]}
 {description: **RAM:** $ram MB / $maxRam MB\n**Серверов:** $serverCount \n**Пользователей:** $allmemberscount \n**Команд:** $getvar[commands] \n**Из них ошибок:** $getvar[commandsError]}
-{color:$replaceText[$userRoleColor[$clientID];#;]}};975806804716032020]
+{color:$getvar[color]}};975806804716032020]
 $suppressErrors`,
 executeOnStartup: true,
 every: 50000
