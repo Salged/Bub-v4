34f45e3878034593ab4b6c681d4812af67a5d826
Salged <1212discord@gmail.com>
2026-07-25 23:15:38 +0300

    Update болотный буб2.js

--- commands/Closet/болотный буб2.js
+++ commands/Closet/болотный буб2.js
@@ -3,7 +3,7 @@ name: "3️⃣3️⃣",
 type: "interaction",
 prototype: "button",
 code: `$setuservar[img;$getuservar[скин_$get[скин]]]
-$interactionReply[{newEmbed:{title:Вы успешно одели скин #3️⃣}{image:$getuservar[скин_$get[скин]]}{color:$getvar[color]}}]
+$interactionReply[{newEmbed:{title:Вы успешно надели скин #3️⃣}{image:$getuservar[скин_$get[скин]]}{color:$getvar[color]}}]
 
 $onlyif[$getuservar[скин_$get[скин]]!=;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:У вас не куплен данный скин}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
 $let[скин;3]`
