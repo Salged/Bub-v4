74d5cfa9ef7fc368b033e48939f487973d73f9d0
Salged <1212discord@gmail.com>
2026-07-25 22:14:23 +0300

    Update botJoins.js

--- handler/botJoins.js
+++ handler/botJoins.js
@@ -1,17 +1,22 @@
-module.exports = (bot) => { 
-bot.guildJoinCommand({
+module.exports = (bot) => [{ 
+name "guild join"
+type: "guildJoin",
 channel: "$getvar[servers]",
-    code: `$title[1;Бота добавили на новый сервер] $description[1;Сервер: $servername | $guildid
+code: `$title[1;Бота добавили на новый сервер] $description[1;Сервер: $guildname | $guildid
 Владелец: $usertag[$ownerid]
-Пользователей: $memberscount] $color[1;GREEN]
-$onlyif[$getservervar[blacklist_server]==false;{execute:bot_leave}]` });
-bot.awaitedCommand({
+Пользователей: $memberscount] 
+$color[1;GREEN]
+$onlyif[$getservervar[blacklist_server]==false;{execute:bot_leave}]` },
+{
+type: "awaited",
 name: "bot_leave",
-code: `$botLeave`
-})
-bot.guildLeaveCommand({
+code: `$clientLeave`
+},
+{
+    name: "guildLeave",
+    type: "guildLeave",
     channel: "$getvar[servers]",
     code: `$title[1;Бота убрали с сервера] $description[1;Сервер: $servername | $guildid
 Владелец: $usertag[$ownerid]
-Пользователей: $memberscount] $color[1;RED]`    })
-}
+Пользователей: $memberscount] $color[1;RED]`
+}]
