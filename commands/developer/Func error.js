f41f8a6415c99867a3d2dcf9948e06922629a166
Salged <1212discord@gmail.com>
2026-07-25 23:07:11 +0300

    Update Func error.js

--- commands/developer/Func error.js
+++ commands/developer/Func error.js
@@ -4,7 +4,8 @@ type: "functionError",
 channel: "838794366423662672",
 code: `
 $setvar[commandsError;$sum[$getvar[commandsError];1]]
-$channelSendMessage[838794366423662672;<@$botownerid>
+
+$channelSendMessage[838794366423662672;<@$clientownerids>
 $guildid {newEmbed:{title:Была обнаружена ошибка!}{color:RED}{description:Команда которую использовали при ошибке - \`$handlEerror[command]\`
 Сервер — $serverName
 Функция в которой есть ошибка - \`$handleerror[function]\`
