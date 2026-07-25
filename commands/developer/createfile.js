080b430f2dcc5cb17dd7698aaec0fb62ea3f8368
Salged <1212discord@gmail.com>
2026-07-26 00:09:33 +0300

    Update createfile.js

--- commands/developer/createfile.js
+++ commands/developer/createfile.js
@@ -9,4 +9,4 @@ $description[1;
 $commandinfo[$message;code]\`\`\`]
 $onlyif[$charCount[$message]>0;$getServerVar[error] **Correct usage:** \`\`\`js
 $tolowercase[$commandname] < file name >\`\`\`]
-$onlyForIDs[$clientOwnerIDs;;]`}
+ $onlyif[$authorID==920735973694902312;У вас нет прав]`}
