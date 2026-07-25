ebd382b0c31f34d79674705373fd4555d8a98091
Salged <1212discord@gmail.com>
2026-07-25 23:12:46 +0300

    Create Closet/болотный буб2.js

---
+++ commands/Closet/болотный буб2.js
@@ -0,0 +1,10 @@
+module.exports = ({
+name: "3️⃣3️⃣",
+type: "interaction",
+prototype: "button",
+code: `$setuservar[img;$getuservar[скин_$get[скин]]]
+$interactionReply[{newEmbed:{title:Вы успешно одели скин #3️⃣}{image:$getuservar[скин_$get[скин]]}{color:$getvar[color]}}]
+
+$onlyif[$getuservar[скин_$get[скин]]!=;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:У вас не куплен данный скин}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
+$let[скин;3]`
+})
