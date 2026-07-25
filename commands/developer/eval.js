a7f07c7a2692a425998477864794eec1df49895d
Salged <1212discord@gmail.com>
2026-07-26 00:09:12 +0300

    Update eval.js

--- commands/developer/eval.js
+++ commands/developer/eval.js
@@ -1,6 +1,7 @@
 module.exports = ({
   name: "e",
 code: `
-$eval[$message;no;yes] $onlyForIDs[clientownerids;net]
+$eval[$message;no;yes] 
+ $onlyif[$authorID==920735973694902312;У вас нет прав]
 `
 })
