04a09b5b98de67d85fa1d5744a8f6892a1499a15
Salged <1212discord@gmail.com>
2026-07-25 23:01:19 +0300

    Update Дракон Буб .js

--- commands/Skins/Дракон Буб .js
+++ commands/Skins/Дракон Буб .js
@@ -2,7 +2,7 @@ module.exports = [{
 name: "2️⃣",
 type: "interaction",
 prototype: "button",
-code: `$interactionReply[;{newEmbed:{title:Скин #2️⃣}{color:$getvar[color]}{image:https://media.discordapp.net/attachments/839587792786161664/951836635551637514/IMG_20220311_093636.jpg}}{actionRow:{button:Купить скин:2:дракон}};;;true]
+code: `$interactionReply[{newEmbed:{title:Скин #2️⃣}{color:$getvar[color]}{image:https://media.discordapp.net/attachments/839587792786161664/951836635551637514/IMG_20220311_093636.jpg}}{actionRow:{button:Купить скин:2:дракон}};;;true]
 
 $onlyif[$getglobaluservar[blacklist]==false;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Вы заблокированы, обратитесь на [сервер поддержки]($getvar[invite]) для оказания вам помощи}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]`
 }, {
