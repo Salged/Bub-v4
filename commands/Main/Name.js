559cbcdced033c22a9ff0abebf90e2634e5cb992
Salged <1212discord@gmail.com>
2026-07-25 22:42:36 +0300

    Update Name.js

--- commands/Main/Name.js
+++ commands/Main/Name.js
@@ -3,10 +3,16 @@ name: "name",
 type: "interaction",
 prototype: "slash",
 code: `$setvar[commands;$sum[$getvar[commands];1]]
-$channelsendmessage[$getvar[logchannel];$guildid {newEmbed:{title: Использована новая команда}{description:**Сервер:** $servername  \n**Пользователь:** $usertag | $authorid \n**Команда:** $commandname}}]
+
+$channelsendmessage[$getvar[logchannel];$guildid {newEmbed:{title: Использована новая команда}{description:**Сервер:** $guildname $guildid  \n**Пользователь:** $usertag | $authorid \n**Команда:** $commandname}}]
+
 $setuservar[name;$replacetext[$interactiondata[options.data[0].value];*;]]
+
 $interactionReply[Хорошо, теперь это — **$replacetext[$interactiondata[options.data[0].value];*;]** <:bubkrytoi:838837227609522226>]
+
 $onlyif[$charCount[$interactiondata[options.data[0].value]]<16;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Нельзя указать более 15 символов в нике, сейчас их \`$charCount[$interactiondata[options.data[0].value]]\`}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
+
 $onlyif[$getuservar[name]!=$interactiondata[options.data[0].value];{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:У вас уже указан данный ник}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
+
 $onlyif[$getglobaluservar[blacklist]==false;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Вы заблокированы, обратитесь на [сервер поддержки]($getvar[invite]) для оказания вам помощи}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]`
 })
