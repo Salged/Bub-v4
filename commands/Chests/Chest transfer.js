754910144ca38709e23e42aee822da003058aeff
Salged <1212discord@gmail.com>
2026-07-26 12:00:44 +0300

    Update Chest transfer.js

--- commands/Chests/Chest transfer.js
+++ commands/Chests/Chest transfer.js
@@ -6,14 +6,19 @@ code: `$setvar[commands;$sum[$getvar[commands];1]]
 
 $channelsendmessage[$getvar[logchannel];{newEmbed:{title: Использована новая команда}{description:**Сервер:** $guildname | $guildid \n**Пользователь:** $usertag | $authorid \n**Команда:** $commandname}}]
 
-$interactionReply[Вы передали <@$interactionData[options.data[0].options[0].value]> \`$replacetext[$replacetext[$replacetext[$interactionData[options.data[0].options[1].value];1;Обычный];2;Редкий];3;Эпический]\` сундук $customemoji[q_chest_$interactionData[options.data[0].options[1].value]]
+$interactionReply[Вы передали <@$get[user]> \`$replacetext[$replacetext[$replacetext[$interactionData[options.data[0].options[1].value];1;Обычный];2;Редкий];3;Эпический]\` сундук $customemoji[q_chest_$get[chest]]
 
-$setuservar[chest_$interactionData[options.data[0].options[1].value];$sub[$getuservar[chest_$interactionData[options.data[0].options[1].value]];1]]
-$setuservar[chest_$interactionData[options.data[0].options[1].value];$sum[$getuservar[chest_$interactionData[options.data[0].options[1].value];$interactionData[options.data[0].options[0].value]];1];$interactionData[options.data[0].options[0].value]]
+$setuservar[chest_$get[chest];$sub[$getuservar[chest_$get[chest];1]]
+$setuservar[chest_$get[chest];$sum[$getuservar[chest_$get[chest];$get[user]];1];$get[user]]
 
-$onlyif[$getuservar[chest_$interactionData[options.data[0].options[1].value]]>=1;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:У вас нет сундуков данного типа}{color:$getvar[color_error]}}{ephemeral}{interaction}]
+$onlyif[$getuservar[chest_$get[chest]]>=1;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:У вас нет сундуков данного типа}{color:$getvar[color_error]}}{ephemeral}{interaction}]
 
-$onlyif[$memberexists[$interactionData[options.data[0].options[0].value]]==true;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Пользователя нет на сервере}{color:$getvar[color_error]}}{ephemeral}{interaction}]
+$onlyif[$get[user]!=$interactiondata[author.id];{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Нельзя передавать сундуки самому себе}{color:$getvar[color_error]}}{ephemeral}{interaction}]
+
+$onlyif[$memberexists[$get[user]]==true;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Пользователя нет на сервере}{color:$getvar[color_error]}}{ephemeral}{interaction}]
+
+$let[chest;$interactionData[options.data[0].options[1].value]]
+$let[user;$interactionData[options.data[0].options[0].value]]
 
 $onlyif[$getglobaluservar[blacklist]==false;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Вы заблокированы, обратитесь на [сервер поддержки]($getvar[invite]) для оказания вам помощи}{color:$getvar[color_error]}}{ephemeral}{interaction}]
 
