8f1cdc2ac8059b2f2d03c63400966b72bee2854a
Salged <1212discord@gmail.com>
2026-07-26 13:26:55 +0300

    Update Leaderboard.js

--- commands/Main/Leaderboard.js
+++ commands/Main/Leaderboard.js
@@ -6,7 +6,9 @@ code: `$setvar[commands;$sum[$getvar[commands];1]]
 
 $channelsendmessage[$getvar[logchannel];$guildid {newEmbed:{title: Использована новая команда}{description:**Сервер:** $guildname | $guildid  \n**Пользователь:** $usertag | $authorid \n**Команда:** $commandname}}]
 
-$interactionReply[{newEmbed:{title:Таблица лидеров сервера по $if[$get[type]==bank;деньгам на счету;деньгам на руках}{description:$replacetext[$replacetext[$replacetext[$userLeaderboard[$guildid;$get[type];asc;{top} - {username}: {value}$getvar[wallet]];1 -;🥇 -;1];2 -;🥈 -;1];3 -;🥉 -;1]}{color:$getvar[color]}}]
+$interactionReply[{newEmbed:{title:Таблица лидеров сервера по $if[$get[type]==bank;деньгам на счету;деньгам на руках]}{description:$replacetext[$replacetext[$replacetext[$userLeaderboard[$guildid;$get[type];asc;{top} - {username}: {value}$getvar[wallet]];1 -;🥇 -;1];2 -;🥈 -;1];3 -;🥉 -;1]}{color:$getvar[color]}}]
+
+$let[lb;$replaceText[$replacetext[Таблица лидеров по $get[type];bank;деньгам на счету];korm;деньгам на балансе]]
 
 $onlyif[$userLeaderboard[$guildid;$get[type];asc;{top} - {username}: {value}$getvar[wallet]]!=;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title:Произошла ошибка!}{description:Данная таблица лидеров пуста}{color:$getvar[color_error]}}{ephemeral}{interaction}]
 
