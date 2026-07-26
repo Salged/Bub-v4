8b409978feea7502b9acf011aa2da9c67952bdc6
Salged <1212discord@gmail.com>
2026-07-26 14:52:49 +0300

    Update Очистить.js

--- commands/Types/Mine/Очистить.js
+++ commands/Types/Mine/Очистить.js
@@ -7,11 +7,11 @@ $setuservar[mine_выбор;]
 $setuservar[korm;$sum[$getuservar[korm];$random[1000;3000]]]
 $setuservar[xp;$sum[$getuservar[xp];$random[10;30]]] 
 
-$onlyif[$getuservar[mine_выбор]==очистить;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Вы должны были выбрать $replacetext[$replacetext[$replacetext[$getuservar[mine_выбор];добыть;⛏️];взорвать;🧨];очистить;🧹]}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
+$onlyif[$getuservar[mine_выбор]==очистить;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Вы должны были выбрать $replacetext[$replacetext[$replacetext[$getuservar[mine_выбор];добыть;⛏️];взорвать;🧨];очистить;🧹]}{color:$getvar[color_error]}}{ephemeral}{interaction}]
 
-$onlyif[$getuservar[mine_выбор]!=;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:На данный момент для вас нет работы!}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
+$onlyif[$getuservar[mine_выбор]!=;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:На данный момент для вас нет работы!}{color:$getvar[color_error]}}{ephemeral}{interaction}]
 
-$onlyIf[$advancedTextSplit[$interactionData[customId];_;2]==$interactionData[author.id];{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Не вы вызвали данную команду}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
+$onlyIf[$advancedTextSplit[$interactionData[customId];_;2]==$interactionData[author.id];{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Не вы вызвали данную команду}{color:$getvar[color_error]}}{ephemeral}{interaction}]
 
 $onlyIf[$advancedTextSplit[$interactionData[customId];_;1]==очистить;] 
 
