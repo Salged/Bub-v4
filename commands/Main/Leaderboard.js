f4fc55a83b1eead96a1459f226e664f6cb273b84
Salged <1212discord@gmail.com>
2026-07-26 13:07:13 +0300

    Update Leaderboard.js

--- commands/Main/Leaderboard.js
+++ commands/Main/Leaderboard.js
@@ -8,6 +8,8 @@ $channelsendmessage[$getvar[logchannel];$guildid {newEmbed:{title: Исполь�
 
 $interactionReply[{newEmbed:{title:Таблица лидеров сервера по $if[$get[type]==bank;деньгам на счету;деньгам на руках}{description:$replacetext[$replacetext[$replacetext[$userLeaderboard[$guildid;$get[type];asc;{top} - {username}: {value}$getvar[wallet]];1 -;🥇 -;1];2 -;🥈 -;1];3 -;🥉 -;1]}{color:$getvar[color]}}]
 
+$onlyif[$userLeaderboard[$guildid;$get[type];asc;{top} - {username}: {value}$getvar[wallet]]!=;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title:Произошла ошибка!}{description:Данная таблица лидеров пуста}{color:$getvar[color_error]}}{ephemeral}{interaction}]
+
 $let[type;$interactionData[options.data[0].value]]
 
 $onlyif[$getglobaluservar[blacklist]==false;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title:Произошла ошибка!}{description:Вы заблокированы, обратитесь на [сервер поддержки]($getvar[invite]) для оказания вам помощи}{color:$getvar[color_error]}}{ephemeral}{interaction}]
