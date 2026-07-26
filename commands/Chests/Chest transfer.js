6cf243a9a506b5c2fe25f9a38206a2eb5d1da22d
Salged <1212discord@gmail.com>
2026-07-26 12:18:38 +0300

    Update Chest transfer.js

--- commands/Chests/Chest transfer.js
+++ commands/Chests/Chest transfer.js
@@ -15,6 +15,8 @@ $onlyif[$getuservar[chest_$get[chest]]>=1;{newEmbed:{thumbnail:https://cdn.disco
 
 $onlyif[$get[user]!=$interactiondata[author.id];{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Нельзя передавать сундуки самому себе}{color:$getvar[color_error]}}{ephemeral}{interaction}]
 
+$onlyif[$isbot[$get[user]]==false;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Нельзя передавать сундуки ботам}{color:$getvar[color_error]}}{ephemeral}{interaction}]
+
 $onlyif[$memberexists[$get[user]]==true;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Пользователя нет на сервере}{color:$getvar[color_error]}}{ephemeral}{interaction}]
 
 $let[chest;$interactionData[options.data[0].options[1].value]]
