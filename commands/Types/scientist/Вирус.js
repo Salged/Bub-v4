8572c8a923f82889d83b9669d00a8b1e2a04b84c
Salged <1212discord@gmail.com>
2026-07-26 14:54:47 +0300

    Update Вирус.js

--- commands/Types/scientist/Вирус.js
+++ commands/Types/scientist/Вирус.js
@@ -7,11 +7,11 @@ $setuservar[учёный_выбор;]
 $setuservar[korm;$sum[$getuservar[korm];$random[3500;6000]]]
 $setuservar[xp;$sum[$getuservar[xp];$random[20;45]]] 
 
-$onlyif[$getuservar[учёный_выбор]==вирус;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title:Произошла ошибка!}{description:Вы должны были выбрать $replacetext[$replacetext[$replacetext[$getuservar[учёный_выбор];вакцина;⚗️];расчёт;📊];вирус;🦠]}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
+$onlyif[$getuservar[учёный_выбор]==вирус;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title:Произошла ошибка!}{description:Вы должны были выбрать $replacetext[$replacetext[$replacetext[$getuservar[учёный_выбор];вакцина;⚗️];расчёт;📊];вирус;🦠]}{color:$getvar[color_error]}}{ephemeral}{interaction}]
 
-$onlyif[$getuservar[учёный_выбор]!=;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title:Произошла ошибка!}{description:На данный момент для вас нет работы!}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
+$onlyif[$getuservar[учёный_выбор]!=;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title:Произошла ошибка!}{description:На данный момент для вас нет работы!}{color:$getvar[color_error]}}{ephemeral}{interaction}]
 
-$onlyIf[$advancedTextSplit[$interactionData[customId];_;2]==$interactionData[author.id];{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title:Произошла ошибка!}{description:Не вы вызвали данную команду}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
+$onlyIf[$advancedTextSplit[$interactionData[customId];_;2]==$interactionData[author.id];{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title:Произошла ошибка!}{description:Не вы вызвали данную команду}{color:$getvar[color_error]}}{ephemeral}{interaction}]
 
 $onlyIf[$advancedTextSplit[$interactionData[customId];_;1]==вирус;] 
 `
