0d6b2eb95dd63dd138f445dad59e207d5ea88848
Salged <1212discord@gmail.com>
2026-07-26 22:13:29 +0300

    Update Addrole.js

--- commands/Shop/Addrole.js
+++ commands/Shop/Addrole.js
@@ -1,44 +1,37 @@
-module.exports = ({
-name: "add_role",
-type: "interaction",
-prototype: "slash",
-code: `$setvar[commands;$sum[$getvar[commands];1]]
-
+module.exports = [{
+    name: "add_role",
+    type: "interaction",
+    prototype: "slash",
+    code: `$setvar[commands;$sum[$getvar[commands];1]]
 $channelsendmessage[$getvar[logchannel];{newEmbed:{title: Использована новая команда}{description:**Сервер:** $guildname | $guildid \n**Пользователь:** $usertag | $authorid \n**Команда:** $commandname}}]
+$setservervar[shop_roles;$get[newRoles]]
+$setservervar[shop_prices;$get[newPrices]]
 
- $djsEval[
-            let shop = JSON.parse($getServerVar[shop] || '[]');
-            shop.push({ id: '$get[roleId]', price: Number('$get[price]') });
-            $setServerVar[shop; JSON.stringify(shop)];
-            return '';
-        ]
-        
-$interactionReply[{newEmbed:{title: Добавление роли в магазин}{description:Вы добавили роль <@&$get[roleId]> ценой $get[price]$getvar[wallet] на слот $get[id]}{color:$getglobaluservar[color]}}]
+$interactionReply[{newEmbed:{title: Добавление роли в магазин}{description:Вы добавили роль <@&$get[roleId]> ценой $get[price]$getvar[wallet] в магазин}{color:$getvar[color]}}]
+
+$let[newRoles;$if[$get[currentRoles]==;$get[roleId];$get[currentRoles],$get[roleId]]]
+$let[newPrices;$if[$get[currentPrices]==;$get[price];$get[currentPrices],$get[price]]]
+
+$onlyif[$checkContains[,$get[currentRoles],;,$get[roleId],]==false;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Эта роль уже есть в магазине}{color:$getvar[color_error]}}{ephemeral}{interaction}]
+
+$onlyif[$get[shopCount]<25;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Максимально 25 ролей в магазине (ограничение select menu)}{color:$getvar[color_error]}}{ephemeral}{interaction}]
+
+$let[shopCount;$if[$get[currentRoles]==;0;$sum[$charCount[$get[currentRoles];,];1]]]
+$let[currentPrices;$getservervar[shop_prices]]
+$let[currentRoles;$getservervar[shop_roles]]
 
 $onlyif[$get[price]<500001;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Максимальная цена роли \`500.000\`$getvar[wallet]}{color:$getvar[color_error]}}{ephemeral}{interaction}]
-$onlyif[$get[price]>=1000;"{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Минимальная цена роли \`1.000\`$getvar[wallet]}{color:$getvar[color_error]}}{ephemeral}{interaction}]
 
-$onlyif[$roleposition[$userhighestrole[$clientid]]<$roleposition[$get[roleId]];{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Указанная роль выше роли бота}{color:$getvar[color_error]}}{ephemeral}{interaction}]
-   
-$onlyif[$ismanagedrole[$findrole[$get[roleid]]]==false;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Роль интеграции нельзя добавлять в магазин}{color:$getvar[color_error]}}{ephemeral}{interaction}]
+$onlyif[$get[price]>=1000;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Минимальная цена роли \`1.000\`$getvar[wallet]}{color:$getvar[color_error]}}{ephemeral}{interaction}]
 
+$onlyif[$roleposition[$userhighestrole[$clientid]]>$roleposition[$get[roleId]];{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Указанная роль выше роли бота}{color:$getvar[color_error]}}{ephemeral}{interaction}]
 
-$onlyif[$get[id]<11;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Максимально 10 слотов в магазине!}{color:$getvar[color_error]}}{ephemeral}{interaction}]
+$onlyif[$ismanagedrole[$findrole[$get[roleId]]]==false;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Роль интеграции нельзя добавлять в магазин}{color:$getvar[color_error]}}{ephemeral}{interaction}]
 
-$onlyif[$get[id]>=1;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Укажите слот от 1 до 10 включительно}{color:$getvar[color_error]}}{ephemeral}{interaction}]
-
-$onlyif[$isnumber[$get[id]]==true;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Укажите номер слота!}{color:$getvar[color_error]}}{ephemeral}{interaction}]
-$let[id;$interactionData[options.data[0].value]]
 $let[roleId;$interactionData[options.data[1].value]]
 $let[price;$interactionData[options.data[2].value]]
-        
+
 $onlyperms[manageserver;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Вы не можете управлять сервером}{color:$getvar[color_error]}}{ephemeral}{interaction}]
-
-
-
-$onlyif[$getglobaluservar[blacklist]==false;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Вы заблокированы, обратитесь на [сервер поддержки]($getvar[invite]) для оказания вам помощи}{color:$getvar[color_error]}}{ephemeral}{interaction}]
-
-`
-
-}) 
+$onlyif[$getglobaluservar[blacklist]==false;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Вы заблокированы, обратитесь на [сервер поддержки]($getvar[invite]) для оказания вам помощи}{color:$getvar[color_error]}}{ephemeral}{interaction}]`
+}];
 
