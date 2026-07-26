445c2a00d2cbd119aa992f524051a0ba730e9949
Salged <1212discord@gmail.com>
2026-07-26 13:03:35 +0300

    Update Mine.js

--- commands/Main/Mine.js
+++ commands/Main/Mine.js
@@ -10,9 +10,10 @@ $setuservar[mine_выбор;$randomText[добыть;взорвать;очист
 
 $interactionReply[{newEmbed:{author:Работа в глубокой шахте:$useravatar}{field:Ваша задача#COLON# **$randomText[добыть;взорвать;очистить]**:⛏️ — добыть \n🧨 — взорвать \n🧹 — очистить }{color:$getvar[color]}}{actionRow:{button:⛏️:2:добыть_$authorid}{button:🧨:2:взорвать_$authorid}{button:🧹:2:очистить_$authorid}}{actionRow:{button:Напомнить:1:mineremind_$authorid}}]
 
-$cooldown[30m;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title:Произошла ошибка!}{description:Ваш Буб устал, приходите через %time%}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
+$cooldown[30m;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title:Произошла ошибка!}{description:Ваш Буб устал, приходите через %time%}{color:$getvar[color_error]}}{ephemeral}{interaction}]
 
-$onlyif[$getglobaluservar[blacklist]==false;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title:Произошла ошибка!}{description:Вы заблокированы, обратитесь на [сервер поддержки]($getvar[invite]) для оказания вам помощи}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]`
+$onlyif[$getglobaluservar[blacklist]==false;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title:Произошла ошибка!}{description:Вы заблокированы, обратитесь на [сервер поддержки]($getvar[invite]) для оказания вам помощи}{color:$getvar[color_error]}}{ephemeral}{interaction}]
+`
 }),
 ({
     type: "interaction",
@@ -21,7 +22,7 @@ $onlyif[$getglobaluservar[blacklist]==false;{"embeds": "{newEmbed:{thumbnail:htt
 
 $setTimeout[mine_remind;30m;{ "userID": "$authorid"}]
 
-$onlyIf[$advancedTextSplit[$interactionData[customId];_;2]==$interactionData[author.id];{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title:Произошла ошибка!}{description:Не вы вызвали данную команду!}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
+$onlyIf[$advancedTextSplit[$interactionData[customId];_;2]==$interactionData[author.id];{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title:Произошла ошибка!}{description:Не вы вызвали данную команду!}{color:$getvar[color_error]}}{ephemeral}{interaction}]
 
 $onlyIf[$advancedTextSplit[$interactionData[customId];_;1]==mineremind;]`
     }), ({
