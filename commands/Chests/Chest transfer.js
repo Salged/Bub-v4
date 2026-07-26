8972425d983a74b71c05427520c2f5e941376260
Salged <1212discord@gmail.com>
2026-07-26 12:09:59 +0300

    Update Chest transfer.js

--- commands/Chests/Chest transfer.js
+++ commands/Chests/Chest transfer.js
@@ -8,7 +8,7 @@ $channelsendmessage[$getvar[logchannel];{newEmbed:{title: Использован
 
 $interactionReply[Вы передали <@$get[user]> \`$replacetext[$replacetext[$replacetext[$interactionData[options.data[0].options[1].value];1;Обычный];2;Редкий];3;Эпический]\` сундук $customemoji[q_chest_$get[chest]]
 
-$setuservar[chest_$get[chest];$sub[$getuservar[chest_$get[chest];1]]
+$setuservar[chest_$get[chest];$sub[$getuservar[chest_$get[chest]];1]]
 $setuservar[chest_$get[chest];$sum[$getuservar[chest_$get[chest];$get[user]];1];$get[user]]
 
 $onlyif[$getuservar[chest_$get[chest]]>=1;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:У вас нет сундуков данного типа}{color:$getvar[color_error]}}{ephemeral}{interaction}]
