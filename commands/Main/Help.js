b65812557fef4da73231245cb20678bee5c27591
Salged <1212discord@gmail.com>
2026-07-26 12:41:59 +0300

    Update Help.js

--- commands/Main/Help.js
+++ commands/Main/Help.js
@@ -6,7 +6,8 @@ code: `$setvar[commands;$sum[$getvar[commands];1]]
 
 $channelsendmessage[$getvar[logchannel];$guildid {newEmbed:{title: Использована новая команда}{description:**Сервер:** $guildname | $guildid  \n**Пользователь:** $usertag | $authorid \n**Команда:** $commandname}}]
 
-$interactionReply[{newEmbed:{author:Команды Буба:$userAvatar[$clientid]}{field:Администраторам:\`add_role, delete_role, upcoin, uncoin\`}{field:Основные:\`bub, bot, bank, chest, casino, skins, wardrobe\`}{field:Заработок:\`mine, builder, bonus\`}{field:Разное:\`walk, leaderboard, name, pay, buy_role, store, rob\`}{color:$getvar[color]}}]
+$interactionReply[{newEmbed:{author:Команды Буба:$userAvatar[$clientid]}{field:Администраторам:\`add_role, delete_role, upcoin, uncoin\`}{field:Основные:\`bub, bot, bank, chest, casino, skins, wardrobe\`}{field:Заработок:\`mine, builder, bonus, science\`}{field:Разное:\`walk, leaderboard, name, pay, buy_role, store, rob, rank\`}{color:$getvar[color]}}]
 
-$onlyif[$getglobaluservar[blacklist]==false;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Вы заблокированы, обратитесь на [сервер поддержки]($getvar[invite]) для оказания вам помощи}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]`
+$onlyif[$getglobaluservar[blacklist]==false;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Вы заблокированы, обратитесь на [сервер поддержки]($getvar[invite]) для оказания вам помощи}{color:$getvar[color_error]}}{ephemeral}{interaction}]
+`
 })
