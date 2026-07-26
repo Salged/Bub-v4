9125c01c0f21f0af373774c371d814ef3818dbc4
Salged <1212discord@gmail.com>
2026-07-26 12:38:05 +0300

    Update Bonus.js

--- commands/Main/Bonus.js
+++ commands/Main/Bonus.js
@@ -11,7 +11,7 @@ $setuservar[xp;$sum[$getuservar[xp];$random[20;40]]]
 
 $interactionReply[Вы получили свои $random[3000;5000]$getvar[wallet] и $random[20;40]⚗️;;{actionRow:{button: Напомнить:1:bonusremind_$authorid}}]
 
-$cooldown[12h;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Ваш Буб устал, приходите через %houf%ч. %min%м.}{color:$getvar[color_error]}}{ephemeral}{interaction}]
+$cooldown[12h;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Ваш Буб устал, приходите через %hour%ч. %min%м.}{color:$getvar[color_error]}}{ephemeral}{interaction}]
 
 $onlyif[$getglobaluservar[blacklist]==false;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Вы заблокированы, обратитесь на [сервер поддержки]($getvar[invite]) для оказания вам помощи}{color:$getvar[color_error]}}{ephemeral}{interaction}]
 `
