4aa731fb30dcdae3af7ba9f908bacd9cdbbeebc0
Salged <1212discord@gmail.com>
2026-07-26 13:49:44 +0300

    Update scientist.js

--- commands/Main/scientist.js
+++ commands/Main/scientist.js
@@ -11,7 +11,7 @@ $interactionReply[{newEmbed:{author:Работа в лаборатории:$user
 
 $cooldown[70m;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Ваш Буб устал, приходите через %time%}{color:$getvar[color_error]}}{ephemeral}{interaction}]
 
-$onlyif[$getuservar[lvl]>=4;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:У вашего Буба должен быть 4 уровень и выше. Сейчас у вас: $getuservar[lvl] уровень}{color:$getvar[color_error]}}{ephemeral}{interaction}]
+$onlyif[$getuservar[lvl]>=4;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:У вашего Буба должен быть 4 уровень и выше. Сейчас - $getuservar[lvl] уровень}{color:$getvar[color_error]}}{ephemeral}{interaction}]
 
 $onlyif[$getglobaluservar[blacklist]==false;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Вы заблокированы, обратитесь на [сервер поддержки]($getvar[invite]) для оказания вам помощи}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]`
 },
