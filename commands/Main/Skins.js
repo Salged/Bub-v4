2d505ef3ae63db2dfd517ca4a91505311a2f1b5f
Salged <1212discord@gmail.com>
2026-07-26 16:48:39 +0300

    Update Skins.js

--- commands/Main/Skins.js
+++ commands/Main/Skins.js
@@ -18,9 +18,10 @@ $onlyif[$getglobaluservar[blacklist]==false;{newEmbed:{thumbnail:https://cdn.dis
     prototype: "selectMenu",
     code: `
         $interactionReply[{newEmbed:{title:Скин: $get[skinName]}{description:Цена: **$get[skinPrice]**$getvar[wallet]}{image:$get[skinImage]}{color:$getvar[color]}}{actionRow:{button:Купить:2:skinbuy_$get[skin]_$authorid:false}{button:Назад:4:skinback_$authorid:false}}]
-        $let[skinImage;$djsEval[require("../handler/skins-data.js")[$get[skin]].image]]
-        $let[skinPrice;$djsEval[require("../handler/skins-data.js")[$get[skin]].price]]
-        $let[skinName;$djsEval[require("../handler/skins-data.js")[$get[skin]].name]]
+        $let[skinData;$djsEval[JSON.stringify(require("../handler/skins-data.js")[data.get("skin")])]]
+        $let[skinImage;$djsEval[JSON.parse(data.get("skinData")).image]]
+        $let[skinPrice;$djsEval[JSON.parse(data.get("skinData")).price]]
+        $let[skinName;$djsEval[JSON.parse(data.get("skinData")).name]]
         $onlyIf[$advancedTextSplit[$interactionData[customId];_;2]==$interactionData[author.id];{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Не вы вызвали данную команду}{color:$getvar[color_error]}}{ephemeral}{interaction}]
         $onlyIf[$advancedTextSplit[$interactionData[customId];_;1]==skinshop;]
         $let[skin;$interactionData[values[0]]]
@@ -37,11 +38,12 @@ $onlyif[$getglobaluservar[blacklist]==false;{newEmbed:{thumbnail:https://cdn.dis
         $setuservar[korm;$sub[$getuservar[korm];$get[skinPrice]]]
         $onlyif[$getuservar[скин_$get[skin]]==;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:У вас уже приобретён данный скин}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
         $onlyif[$getuservar[korm]>=$get[skinPrice];{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:У вас на балансе слишком мало средств}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
-        $let[skinImage;$djsEval[require("../handler/skins-data.js")[$get[skin]].image]]
-        $let[skinPrice;$djsEval[require("../handler/skins-data.js")[$get[skin]].price]]
-        $let[skinName;$djsEval[require("../handler/skins-data.js")[$get[skin]].name]]
+        $let[skinData;$djsEval[JSON.stringify(require("../handler/skins-data.js")[data.get("skin")])]]
+        $let[skinImage;$djsEval[JSON.parse(data.get("skinData")).image]]
+        $let[skinPrice;$djsEval[JSON.parse(data.get("skinData")).price]]
+        $let[skinName;$djsEval[JSON.parse(data.get("skinData")).name]]
         $onlyIf[$advancedTextSplit[$interactionData[customId];_;2]==$interactionData[author.id];{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Не вы вызвали данную команду}{color:$getvar[color_error]}}{ephemeral}{interaction}]
-        $onlyIf[$advancedTextSplit[$interactionData[customId];_;1]==skinshop;]
+        $onlyIf[$advancedTextSplit[$interactionData[customId];_;1]==skinbuy;]
         $let[skin;$advancedTextSplit[$interactionData[customId];_;3]]
     `
 },
@@ -56,7 +58,6 @@ $onlyif[$getglobaluservar[blacklist]==false;{newEmbed:{thumbnail:https://cdn.dis
 {stringInput:Болотный Буб — 50.000:3}:
 {stringInput:Буб с дамами — 100.000:4}:
 {stringInput:Буб с гетто — 150.000:5}}}]
-        $onlyIf[$advancedTextSplit[$interactionData[customId];_;1]==$authorid;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Не вы вызвали данную команду}{color:$getvar[color_error]}}{ephemeral}{interaction}]
-        $onlyIf[$advancedTextSplit[$interactionData[customId];_;1]==skinshop;]
+        $onlyIf[$advancedTextSplit[$interactionData[customId];_;1]==skinback;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Не вы вызвали данную команду}{color:$getvar[color_error]}}{ephemeral}{interaction}]
     `
 }];
