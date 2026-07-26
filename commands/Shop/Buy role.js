d51ab1d9d5b97897a42f94732c67e078455956dd
Salged <1212discord@gmail.com>
2026-07-26 21:11:51 +0300

    Update Buy role.js

--- commands/Shop/Buy role.js
+++ commands/Shop/Buy role.js
@@ -18,15 +18,15 @@ $onlyif[$roleposition[$highestrole[$clientid]]<$roleposition[$getservervar[ро�
 
 $onlyif[$roleexists[$getservervar[роль_$get[role]]]==true;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Роли которую я должен выдать не существует }{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
 
-$let[role;$interactionData[options.data[0].value]]
 $onlyif[$interactionData[options.data[0].value]<11;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Максимально 10 слотов в магазине!}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
 
 $onlyif[$interactionData[options.data[0].value]>=1;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Укажите слот от 1 до 10 включительно}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
 
 $onlyif[$isnumber[$interactionData[options.data[0].value]]==true;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Укажите номер слота!}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
 
-$onlyBotperms[manageroles;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Я не могу выдать вам роль!}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
-
+$onlyBotperms[manageroles;{{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Я не могу выдать вам роль!}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
+$let[roleId;$interactionData[options.data[1].value]]
+$let[price;$interactionData[options.data[2].value]]
 $onlyif[$getglobaluservar[blacklist]==false;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Вы заблокированы, обратитесь на [сервер поддержки]($getvar[invite]) для оказания вам помощи}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
 `
 
