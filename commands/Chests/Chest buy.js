7514459b835d4b0f60a911360672d109a62c8558
Salged <1212discord@gmail.com>
2026-07-26 21:13:21 +0300

    Update Chest buy.js

--- commands/Chests/Chest buy.js
+++ commands/Chests/Chest buy.js
@@ -12,7 +12,7 @@ $setuservar[chest_$get[type_chest];$sum[$getuservar[chest_$get[type_chest]];$int
 
 $setuservar[korm;$sub[$getuservar[korm];$get[price_full]]]
 
-$onlyif[$getuservar[korm]>=$get[price_full];{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:У вас $getuservar[korm]/$get[price_full]$getvar[wallet]}{color:ff0000}}{ephemeral}{interaction}]
+$onlyif[$getuservar[korm]>=$get[price_full];{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:У вас $getuservar[korm]/$get[price_full]$getvar[wallet]}{color:$getvar[color_error]}}{ephemeral}{interaction}]
 
 $let[price_full;$multi[$get[price];$interactionData[options.data[0].options[1].value]]]
 $let[price;$multi[$get[type_chest];3000]]
@@ -20,7 +20,7 @@ $let[price;$multi[$get[type_chest];3000]]
 
 $let[type_chest;$interactionData[options.data[0].options[0].value]]
 
-$onlyif[$getglobaluservar[blacklist]==false;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Вы заблокированы, обратитесь на [сервер поддержки]($getvar[invite]) для оказания вам помощи}{color:ff0000}}{ephemeral}{interaction}]
+$onlyif[$getglobaluservar[blacklist]==false;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Вы заблокированы, обратитесь на [сервер поддержки]($getvar[invite]) для оказания вам помощи}{color:$getvar[color_error]}}{ephemeral}{interaction}]
 $onlyif[$interactionData[options._subcommand]==buy;]
 `
 })
