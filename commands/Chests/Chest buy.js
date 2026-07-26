f08838ac11feea354773bbfcedddd8092a18e73f
Salged <1212discord@gmail.com>
2026-07-26 11:10:13 +0300

    Update Chest buy.js

--- commands/Chests/Chest buy.js
+++ commands/Chests/Chest buy.js
@@ -2,24 +2,11 @@ module.exports = ({
 name: "chest",
 type: "interaction",
 prototype: "slash",
-code: `$setvar[commands;$sum[$getvar[commands];1]]
+code: `
 
-$channelsendmessage[$getvar[logchannel];{newEmbed:{title: Использована новая команда}{description:**Сервер:** $guildname | $guildid \n**Пользователь:** $usertag | $authorid \n**Команда:** $commandname}}]
-
-$interactionReply[Вы купили **$interactionData[options.data[0].options[1].value]** $customEmoji[q_chest_$get[type_chest]] за $get[price_full]$getservervar[wallet]]
-$setuservar[chest_$get[type_chest];$sum[$getuservar[chest_$get[type_chest]];$interactionData[options.data[0].options[1].value]]]
-
-$setuservar[korm;$sub[$getuservar[korm];$get[price_full]]]
-
-$onlyif[$getuservar[korm]>=$get[price_full];{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:У вас $getuservar[korm]/$get[price_full]$getservervar[wallet]}{color:ff0000}}{ephemeral}{interaction}]
-
-$let[price_full;$multi[$get[price];$interactionData[options.data[0].options[1].value]]]
-$let[price;$multi[$get[type_chest];3000]]
+$interactionReply[Вы купили **$interactionData[options.data[0].options[1].value]** $customEmoji[q_chest_$interactionData[options.data[0].options[0].value]] за $getservervar[wallet]]
 
 
-$let[type_chest;$interactionData[options.data[0].options[0].value]]
-
-$onlyif[$getglobaluservar[blacklist]==false;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Вы заблокированы, обратитесь на [сервер поддержки]($getvar[invite]) для оказания вам помощи}{color:ff0000}}{ephemeral}{interaction}]
 $onlyif[$interactionData[options._subcommand]==buy;]`
 
 })
