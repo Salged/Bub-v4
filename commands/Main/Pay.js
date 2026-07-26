71ca764205cc64b35b5a49d84b62188c66f72d62
Salged <1212discord@gmail.com>
2026-07-26 13:15:55 +0300

    Update Pay.js

--- commands/Main/Pay.js
+++ commands/Main/Pay.js
@@ -11,19 +11,20 @@ $setuservar[korm;$sub[$getuservar[korm];$interactiondata[options.data[1].value]]
 
 $interactionReply[{newEmbed:{title:Передача денег}{description:$usertag передал $interactiondata[options.data[1].value]$getvar[wallet] пользователю <@$interactiondata[options.data[0].value]>}{color:$getvar[color]}}]
 
-$onlyif[$getuservar[korm]>=$interactionData[options.data[1].value];{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:На вашем балансе нет столько денег}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
+$onlyif[$getuservar[korm]>=$interactionData[options.data[1].value];{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:На вашем балансе нет столько денег}{color:$getvar[color_error]}}{ephemeral}{interaction}]
 
-$onlyif[$interactionData[options.data[1].value]<1000001;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Укажите число до 1.000.000}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
+$onlyif[$interactionData[options.data[1].value]<1000001;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Укажите число до 1.000.000}{color:$getvar[color_error]}}{ephemeral}{interaction}]
 
-$onlyif[$interactionData[options.data[1].value]>=10;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Укажите число больше 10}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
+$onlyif[$interactionData[options.data[1].value]>=10;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Укажите число больше 10}{color:$getvar[color_error]}}{ephemeral}{interaction}]
 
-$onlyif[$isnumber[$interactionData[options.data[1].value]]==true;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Укажите число!}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
+$onlyif[$isnumber[$interactionData[options.data[1].value]]==true;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Укажите число!}{color:$getvar[color_error]}}{ephemeral}{interaction}]
 
-$onlyif[$isbot[$interactiondata[options.data[0].value]]==false;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Нельзя передать деньги боту}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
+$onlyif[$isbot[$interactiondata[options.data[0].value]]==false;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Нельзя передать деньги боту}{color:$getvar[color_error]}}{ephemeral}{interaction}]
 
-$onlyif[$memberexists[$interactiondata[options.data[0].value]]==true;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Пользователя нет на сервере}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
+$onlyif[$memberexists[$interactiondata[options.data[0].value]]==true;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Пользователя нет на сервере}{color:$getvar[color_error]}}{ephemeral}{interaction}]
 
-$onlyif[$interactiondata[options.data[0].value]!=$authorid;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Нельзя передать деньги самому себе}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
+$onlyif[$interactiondata[options.data[0].value]!=$authorid;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Нельзя передать деньги самому себе}{color:$getvar[color_error]}}{ephemeral}{interaction}]]
 
-$onlyif[$getglobaluservar[blacklist]==false;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Вы заблокированы, обратитесь на [сервер поддержки]($getvar[invite]) для оказания вам помощи}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]`
+$onlyif[$getglobaluservar[blacklist]==false;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Вы заблокированы, обратитесь на [сервер поддержки]($getvar[invite]) для оказания вам помощи}{color:$getvar[color_error]}}{ephemeral}{interaction}]
+`
     })
