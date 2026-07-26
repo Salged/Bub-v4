03946899a11849c6bb7034bb63887e615dfa0a15
Salged <1212discord@gmail.com>
2026-07-26 13:08:56 +0300

    Update Name.js

--- commands/Main/Name.js
+++ commands/Main/Name.js
@@ -10,9 +10,10 @@ $setuservar[name;$replacetext[$interactiondata[options.data[0].value];*;]]
 
 $interactionReply[Хорошо, теперь это — **$replacetext[$interactiondata[options.data[0].value];*;]** <:bubkrytoi:838837227609522226>]
 
-$onlyif[$charCount[$interactiondata[options.data[0].value]]<16;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Нельзя указать более 15 символов в нике, сейчас их \`$charCount[$interactiondata[options.data[0].value]]\`}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
+$onlyif[$charCount[$interactiondata[options.data[0].value]]<33;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Нельзя указать более 32 символов в нике, сейчас их \`$charCount[$interactiondata[options.data[0].value]]\`}{color:$getvar[color_error]}}{ephemeral}{interaction}]
 
-$onlyif[$getuservar[name]!=$interactiondata[options.data[0].value];{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:У вас уже указан данный ник}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]
+$onlyif[$getuservar[name]!=$interactiondata[options.data[0].value];{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:У вас уже указан данный ник}{color:$getvar[color_error]}}{ephemeral}{interaction}]
 
-$onlyif[$getglobaluservar[blacklist]==false;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Вы заблокированы, обратитесь на [сервер поддержки]($getvar[invite]) для оказания вам помощи}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]`
+$onlyif[$getglobaluservar[blacklist]==false;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Вы заблокированы, обратитесь на [сервер поддержки]($getvar[invite]) для оказания вам помощи}{color:$getvar[color_error]}}{ephemeral}{interaction}]
+`
 })
