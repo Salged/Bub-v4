f25639cb7d3822c1ee3f39054b73b269fc374620
Salged <1212discord@gmail.com>
2026-07-26 09:33:34 +0300

    Update Chest buy.js

--- commands/Chests/Chest buy.js
+++ commands/Chests/Chest buy.js
@@ -6,18 +6,18 @@ code: `$setvar[commands;$sum[$getvar[commands];1]]
 
 $channelsendmessage[$getvar[logchannel];{newEmbed:{title: Использована новая команда}{description:**Сервер:** $guildname | $guildid \n**Пользователь:** $usertag | $authorid \n**Команда:** $commandname}}]
 
-$interactionReply[Вы купили **$interactionData[options.data[0].options[1].value]** $customEmoji[q_chest_$get[p]] за $get[d]$getservervar[wallet]]
-$setuservar[chest_$get[p];$sum[$getuservar[chest_$get[p]];$interactionData[options.data[0].options[1].value]]]
+$interactionReply[Вы купили **$interactionData[options.data[0].options[1].value]** $customEmoji[q_chest_$get[type_chest]] за $get[price_full]$getservervar[wallet]]
+$setuservar[chest_$get[count];$sum[$getuservar[chest_$get[type_chest]];$interactionData[options.data[0].options[1].value]]]
 
-$setuservar[korm;$sub[$getuservar[korm];$get[d]]]
+$setuservar[korm;$sub[$getuservar[korm];$get[price_full]]]
 
-$onlyif[$getuservar[korm]>=$get[d];{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:У вас $getuservar[korm]/$get[d]$getservervar[wallet]}{color:RED}}{ephemeral}{interaction}]
+$onlyif[$getuservar[korm]>=$get[price_full];{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:У вас $getuservar[korm]/$get[price_full]$getservervar[wallet]}{color:$getvar[color_error]}}{ephemeral}{interaction}]
 
-$let[d;$multi[$get[l];$interactionData[options.data[0].options[1].value]]]
-$let[l;$multi[$get[p];3000]]
+$let[price_full;$multi[$get[l];$interactionData[options.data[0].options[1].value]]]
+$let[price;$multi[$get[type_chest];3000]]
 
 
-$let[p;$interactionData[options.data[0].options[0].value]]
+$let[type_chest;$interactionData[options.data[0].options[0].value]]
 
 $onlyif[$getglobaluservar[blacklist]==false;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Вы заблокированы, обратитесь на [сервер поддержки]($getvar[invite]) для оказания вам помощи}{color:$getvar[color_error]}}{ephemeral}{interaction}]
 $onlyif[$interactionData[options._subcommand]==buy;]`
