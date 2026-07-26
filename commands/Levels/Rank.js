c2355bf64ace7f8af2434716f1ab0167d90be8a8
Salged <1212discord@gmail.com>
2026-07-26 12:17:00 +0300

    Update Rank.js

--- commands/Levels/Rank.js
+++ commands/Levels/Rank.js
@@ -5,7 +5,7 @@ prototype: "slash",
 code: `$setvar[commands;$sum[$getvar[commands];1]]
 $channelsendmessage[$getvar[logchannel];{newEmbed:{title: Использована новая команда}{description:**Сервер:** $guildname | $guildID \n**Пользователь:** $usertag | $authorid \n**Команда:** $commandname}}]
 
-$interactionReply[{newEmbed:{title:Карточка $usertag[$get[id]]}{image:https://vacefron.nl/api/rankcard?username=$replaceText[$username[$get[id]]; ;+]&avatar=$userAvatar[$get[id]]&level=$getUserVar[lvl;$get[id]]&rank=&currentxp=$getUserVar[xp;$get[id]]&nextlevelxp=$getUserVar[sledxp;$get[id]]&previouslevelxp=0&custombg=https://i.imgur.com/EBPIwMZ.jpeg&xpcolor=$getvar[color]&isboosting=$isBoosting[$get[id]]}{color:$getvar[color]}}]
+$interactionReply[https://vacefron.nl/api/rankcard?username=$replaceText[$username[$get[id]]; ;+]&avatar=$userAvatar[$get[id]]&level=$getUserVar[lvl;$get[id]]&rank=&currentxp=$getUserVar[xp;$get[id]]&nextlevelxp=$getUserVar[sledxp;$get[id]]&previouslevelxp=0&custombg=https://i.imgur.com/EBPIwMZ.jpeg&xpcolor=$getvar[color]&isboosting=$isBoosting[$get[id]]]
 $let[id;$replacetext[$interactiondata[options.data[0].value];undefined;$authorid]]
 
 $onlyif[$getglobaluservar[blacklist]==false;{"embeds": "{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Вы заблокированы, обратитесь на [сервер поддержки]($getvar[invite]) для оказания вам помощи}{color:RED}}", "ephemeral" : true, "options" : { "interaction" : true}}]`
