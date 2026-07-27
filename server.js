0fe95f9f7410c01ccf604540762af2f3e3ef33ec
Salged <1212discord@gmail.com>
2026-07-27 04:22:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -64,6 +64,6 @@ $textSplit[$message[1];:]`
 
 client.awaitedCommand({
     name: "shop_option",
-    code: `{stringInput:Роль за $splitText[2]$getvar[wallet]:$splittext[1]-$splittext[2]:Нажмите чтобы купить эту роль:false}
+    code: `{stringInput:Роль за $splitText[2]:$splittext[1]-$splittext[2]:Нажмите чтобы купить эту роль:false:$getvar[wallet]}
 $textSplit[$message[1];:]`
 });
