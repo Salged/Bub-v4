05eec0b93e6f1d59a8a848d180763c419706b141
Salged <1212discord@gmail.com>
2026-07-27 13:48:37 +0300

    Update Skins.js

--- commands/Main/Skins.js
+++ commands/Main/Skins.js
@@ -5,11 +5,11 @@ module.exports = [{
     code: `$setvar[commands;$sum[$getvar[commands];1]]
 $channelsendmessage[$getvar[logchannel];{newEmbed:{title: Использована новая команда}{description:**Сервер:** $guildname | $guildid \n**Пользователь:** $usertag | $authorid \n**Команда:** $commandname}}]
 $interactionReply[{newEmbed:{title:Магазин скинов Буба}{thumbnail:$userAvatar}{description:Выберите скин из списка ниже, чтобы посмотреть превью и цену}{color:$getvar[color]}}{actionRow:{selectMenu:skinshop_$authorid: Выберите скин:1:1: false:
-{stringInput:Зимний Буб — 20.000:1}:
-{stringInput:Дракон Буб — 35.000:2}:
-{stringInput:Болотный Буб — 50.000:3}:
-{stringInput:Буб с дамами — 100.000:4}:
-{stringInput:Буб с гетто — 150.000:5}}}]
+{stringInput:Зимний Буб — 20.000:one}:
+{stringInput:Дракон Буб — 35.000:two}:
+{stringInput:Болотный Буб — 50.000:theee}:
+{stringInput:Буб с дамами — 100.000:four}:
+{stringInput:Буб с гетто — 150.000:five}}}]
 $onlyif[$getglobaluservar[blacklist]==false;{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description:Вы заблокированы, обратитесь на [сервер поддержки]($getvar[invite]) для оказания вам помощи}{color:$getvar[color_error]}}{ephemeral}{interaction}]`
 },
 {
@@ -25,7 +25,9 @@ $onlyif[$getglobaluservar[blacklist]==false;{newEmbed:{thumbnail:https://cdn.dis
 $textSplit[$if[$get[skin]==1;Зимний Буб,20000,https://media.discordapp.net/attachments/839587792786161664/964163003874422794/2022-04-14-16-56-20.jpg;$if[$get[skin]==2;Дракон Буб,35000,https://media.discordapp.net/attachments/839587792786161664/951836635551637514/IMG_20220311_093636.jpg;$if[$get[skin]==3;Болотный Буб,50000,https://media.discordapp.net/attachments/937714691105517619/937715867171557396/PicsArt_01-31-05.23.15.jpg;$if[$get[skin]==4;Буб с дамами,100000,https://media.discordapp.net/attachments/838794366423662672/937916298254499850/DqHvdW7.jpg;Буб с гетто,150000,https://media.discordapp.net/attachments/838794366423662672/937916757602074624/TItC7rv.jpg]]]];,]
         $onlyIf[$advancedTextSplit[$interactionData[customId];_;2]==$interactionData[author.id];{newEmbed:{thumbnail:https://cdn.discordapp.com/emojis/606562703917449226.gif?v=1&size=4096}{title: Произошла ошибка!}{description: Не вы вызвали данную команду}{color:$getvar[color_error]}}{ephemeral}{interaction}]
         $onlyIf[$advancedTextSplit[$interactionData[customId];_;1]==skinshop;]
-        $let[skin;$interactionData[data.values[0]]]
+
+$let[skin;$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$get[skin1];one;1];two;2];theee;3];four;4];five;5]]
+      $let[skin1;$interactionData[data.values[0]]]
     `
 },
 {
